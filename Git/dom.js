d1553952f4778f8aed6d5c0603c352cd51fe2c60
Mangesh50 <mangeshtapre53@gmail.com>
2023-05-18 00:13:43 +0530

    dom classname

--- Git/dom.js
+++ Git/dom.js
@@ -24,16 +24,16 @@ headerTitle.style.borderBottom = 'solid 3px #000';
 var items = document.getElementsByClassName('list-group-item');
 console.log(items);
 console.log(items[1]);
-items[1].textContent = 'Hello 2';
-items[1].style.fontWeight = 'bold';
-items[1].style.backgroundColor = 'green';
+items[2].textContent = 'Hello 2';
+items[2].style.fontWeight = 'bold';
+items[2].style.backgroundColor = 'green';
 
 //items.style.backgroundColor = '#f4f4f4';
 
-// for(var i = 0; i < items.length;i++)
-// {
-//     items[i].style.backgroundColor = '#f4f4f4';
-// }
+for(var i = 0; i < items.length;i++)
+{
+    items[i].style.fontWeight = 'bold';
+}
 
 // var li = document.getElementsByClassName('list-group-item');
 // console.log(li);
