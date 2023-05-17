49e1bfab621e15526de02c09caaef394d3235b54
Mangesh50 <mangeshtapre53@gmail.com>
2023-05-18 02:58:05 +0530

    dom queryselectorall

--- Git/dom.js
+++ Git/dom.js
@@ -49,38 +49,40 @@
 //     li[i].style.fontWeight = 'bold';
 // }
 //Query Selector //
-var header  = document.querySelector('#main-header');
-header.style.borderBottom = 'solid 4px #ccc';
+// var header  = document.querySelector('#main-header');
+// header.style.borderBottom = 'solid 4px #ccc';
 
-var input = document.querySelector('input');
-input.value = 'Hello World';
+// var input = document.querySelector('input');
+// input.value = 'Hello World';
 
-var submit = document.querySelector('input[type="submit"]');
-submit.value="SEND"
+// var submit = document.querySelector('input[type="submit"]');
+// submit.value="SEND"
 
-var item = document.querySelector('.list-group-item');
-item.style.color = 'red';
+// var item = document.querySelector('.list-group-item');
+// item.style.color = 'red';
 
-var lastItem = document.querySelector('.list-group-item:last-child');
-lastItem.style.color = 'blue';
+// var lastItem = document.querySelector('.list-group-item:last-child');
+// lastItem.style.color = 'blue';
 
-var secondItem = document.querySelector('.list-group-item:nth-child(2)');
-secondItem.style.backgroundColor = 'green';
+// var secondItem = document.querySelector('.list-group-item:nth-child(2)');
+// secondItem.style.backgroundColor = 'green';
 
-var thirdItem = document.querySelector('.list-group-item:nth-child(3)');
-thirdItem.style.display = 'None';
+// var thirdItem = document.querySelector('.list-group-item:nth-child(3)');
+// thirdItem.style.display = 'None';
 
 //QuerySelectorall//
-// var titles = document.querySelectorAll('.title');
+var titles = document.querySelectorAll('.title');
 
-// console.log(titles);
-// titles[0].textContent = 'Hello';
+console.log(titles);
+titles[0].textContent = 'Hello';
 
-// var odd = document.querySelectorAll('li:nth-child(odd)');
-// var even = document.querySelectorAll('li:nth-child(even)');
+var odd = document.querySelectorAll('li:nth-child(odd)');
+//var even = document.querySelectorAll('li:nth-child(even)');
 
-// for(var i = 0;i<odd.length; i++)
-// {
-//     odd[i].style.backgroundColor = '#f4f4f4';
-//     even[i].style.backgroundColor = '#ccc';
-// }
+for(var i = 0;i<odd.length; i++)
+{
+    odd[i].style.backgroundColor = 'green';
+    //even[i].style.backgroundColor = '#ccc';
+}
+var items = document.querySelectorAll('li:nth-child(2)');
+items[0].style.color='green';
