5680b03941a3f7ef5458542314886750f160c03a
Mangesh50 <mangeshtapre53@gmail.com>
2023-05-19 21:08:59 +0530

    scale the app user

---
+++ script.js
@@ -0,0 +1,9 @@
+let myObj = {'key':'value'};
+
+let myObj_serialized = JSON.stringify(myObj);
+
+localStorage.setItem("myObj", myObj_serialized);
+
+let myObj_deserialized = JSON.parse(localStorage.getItem("myObj"));
+
+console.log(myObj_deserialized);
