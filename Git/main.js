2ca0da8d9c82bb877078c07d13401f458d4b7bf9
Mangesh50 <mangeshtapre53@gmail.com>
2023-05-19 18:05:44 +0530

    Store the user

--- Git/main.js
+++ Git/main.js
@@ -82,5 +82,18 @@ function filterItems(e)
 }
 
 //local Storage
-localStorage.setItem('username','Mahesh')
-localStorage.getItem('username')
+//localStorage.setItem('username','Mahesh')
+//localStorage.getItem('username')
+
+let myObj = {
+    name : "Mahesh",
+    age : 27
+};
+
+let myObj_serialized = JSON.stringify(myObj);
+
+localStorage.setItem("myObj", myObj_serialized);
+
+let myObj_deserialized = JSON.parse(localStorage.getItem("myObj"));
+
+console.log(myObj_deserialized);
